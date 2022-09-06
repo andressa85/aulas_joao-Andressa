25f5f28868350a417e519648f06598c52749a628
andressa85 <andressaasouza85@gmail.com>
2022-09-05 22:26:53 -0300

    Aula 4

---
+++ PMII/exemplo3/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from 'react-native';
+import Contador from './components/Contador';
+
+export default function App() {
+  return (
+    <View>
+        <Contador/>
+    </View>
+  );
+}
+
